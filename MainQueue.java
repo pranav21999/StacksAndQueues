789eb5ea5a54ba4ed1147b53928aae6ac09ffd86
pranav21999 <pranavdani99@gmail.com>
2021-07-02 01:01:37 +0530

    added method for inserting element in queue(Enque)

---
+++ MainQueue.java
@@ -0,0 +1,14 @@
+package ds;
+
+public class MainQueue {
+
+	public static void main(String[] args) {
+		// main method
+		Queue queue=new Queue();
+		queue.enque(15);
+		queue.enque(16);
+		queue.enque(17);
+		queue.print();
+	}
+
+}
